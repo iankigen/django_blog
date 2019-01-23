bd7396529455fb69a9783fa69801b1c296108598
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-23 15:38:42 +0300

    chore: get secret from env

--- django_blog/django_blog/settings/base.py
+++ django_blog/django_blog/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
