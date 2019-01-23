96d0f08b78cfd29b1bfba05d9ff1b8965556766b
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-23 15:45:33 +0300

    chore: add article model and migration

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    body = models.TextField()
