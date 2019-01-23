022bebea3a52396b3406785e581451fb35e1dd4b
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-23 15:37:56 +0300

    chore: update default env

--- django_blog/manage.py
+++ django_blog/manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.{}".format(ENVIRONMENT))
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
